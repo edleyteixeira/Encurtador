5ddf7b4c829bee5ffd388161970a4fae9e40853a
Edley Teixeira <75957696+edleyteixeira@users.noreply.github.com>
2022-04-19 13:39:50 -0300

    banco de dados

--- app/admin.py
+++ app/admin.py
@@ -4,10 +4,12 @@ from .models import Pixel, Encurtadas
 
 @admin.register(Encurtadas)
 class EncurtadasAdmin(admin.ModelAdmin):
-    list_display = ('id', 'urlr', 'slug')
+    list_display = ('id', 'urlr', 'name_p', 'slug', 'plataform')
+    list_display_links = ('id', 'urlr', 'slug')
     
 @admin.register(Pixel)
 class PixelAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'pixel_id')
+    list_display_links = ('id', 'name', 'pixel_id')
     
     
--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class Pixel(models.Model):
     plataform = (
@@ -8,7 +8,7 @@ class Pixel(models.Model):
     plataforma = models.CharField(max_length=10 , choices=plataform, blank=False, verbose_name='PIXEL')
     pixel_id = models.CharField(max_length=30, verbose_name='id do pixel', blank=False)
     name = models.CharField(max_length=100, verbose_name='Nome do Pixel', blank=False)
-    #user = models.ForeignKey(EmailAddress, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     
     def __str__(self):
         return self.name
@@ -21,8 +21,9 @@ class Encurtadas(models.Model):
     id = models.AutoField(primary_key=True,verbose_name='id')
     urlr = models.URLField(max_length=200, verbose_name='url original')
     slug = models.CharField(unique=True ,max_length=7, verbose_name='slug da url')
-    plataform = models.ForeignKey(Pixel,on_delete=models.CASCADE)
-    #user = models.ForeignKey(EmailAddress, on_delete=models.CASCADE)
+    plataform = models.ForeignKey(Pixel,on_delete=models.CASCADE, verbose_name='Pixel')
+    name_p = models.CharField(max_length=30, verbose_name='url personalizada')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     
     def __str__(self):
         return self.urlr
